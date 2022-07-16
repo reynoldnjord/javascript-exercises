31fc7c19921cde47da09b6f3b5b321be1b450cf2
reynoldnjord <njordreynold@gmail.com>
2022-07-16 22:23:24 +0300

    Solved (didn't solve 13 because unrelavant)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, pre) => total + pre, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, pre) => total * pre, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  else{
+    return factorial(num-1)*num;
+  }
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    let array = [];
+    for (el in obj){
+        array.push(el.title);
+    }
+    return array;
 };
 
 // Do not edit below this line
